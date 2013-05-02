d37a44beb852d4e17ccea45c4bc9c240c7876e72
Jalu Febry Kesumaninggalih <jal>
2013-05-02 08:12:14 +0700

    Tebak Angka_Final

---
+++ Tebak Angka_final/Tebak Angka/Home.vb
@@ -0,0 +1,16 @@
+﻿Public Class Home
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If ProgressBar1.Value < 100 Then
+            ProgressBar1.Value += 10
+        ElseIf ProgressBar1.Value = 100 Then
+            Timer1.Stop()
+            Me.Hide()
+            Main.Show()
+        End If
+    End Sub
+
+    Private Sub Home_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+End Class
